4e211866f6d9aa9bba7e604988bbb94e4cf64672
Samet YILMAZ <sametyilmaz4444@gmail.com>
2026-07-24 16:23:46 +0300

    Create hooks.py

---
+++ erpnext_ai/hooks.py
@@ -0,0 +1,10 @@
+app_name = "erpnext_ai"
+app_title = "ERPNext AI"
+app_publisher = "PrimeIT"
+app_description = "ERPNext icin AI asistani"
+app_email = "sametyilmaz4444@gmail.com"
+app_license = "MIT"
+
+# Desk arayuzunde her sayfada yuklenir
+app_include_js = "/assets/erpnext_ai/js/copilot.js"
+app_include_css = "/assets/erpnext_ai/css/copilot.css"
